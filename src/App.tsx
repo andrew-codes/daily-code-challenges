39a81bca0525e8686e3c45d49bb2808c3cb971cb
Andrew Smith <andrew@andrew.codes>
2020-02-19 14:04:41 -0500

    getting started
    ability to add code katas as links that provide additional detail when clicked.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import * as React from "react";
-import "./styles.css";
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import CodeKata from './ReactCodeKata'
+import './styles.css'
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+    <Router>
+      <div className="App">
+        <Route exact={true} path="/">
+          <header>
+            <h1>List of Katas</h1>
+          </header>
+        </Route>
+        <CodeKata description="Test" name="test" date={new Date(2020, 2, 19)}>
+          hello
+        </CodeKata>
+      </div>
+    </Router>
+  )
 }
